8b37067842881f8702fca13d43e375fea8c78930
franscwa <g.francisco@knights.ucf.edu>
2022-06-22 16:29:33 -0400

    fixed feedback route

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-import os
 
+from crypt import methods
+import os
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from boto3 import resource
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+FeedbackTable = resource.Table('api-tool-feedback')
+# Homepage
 
 @app.route("/", methods=("GET", "POST"))
 def index():
@@ -16,17 +20,35 @@ def index():
         endpoints = request.form["endpoints"]
         response = openai.Completion.create(
             model="text-davinci-002",
-            #prompt="Code an EXPRESS program with a Postgres database with endpoints for the following GET all countries with stats, PUT new country with stats",
             prompt=generate_prompt(serverlang, dbprovider, endpoints),
             temperature=0.6,
             max_tokens=512,
         )
+
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
+#Sends feedback to the database
+@app.route("/feedback", methods =("POST"))
+def sendFeedbackToDynamo(serverlang, dbprovider, endpoints, feedback):
+   response = FeedbackTable.put_item(
+       Response = {
+           'prompt'     : generate_prompt(serverlang, dbprovider, endpoints),
+           'temperature'  : 0.5,
+           'successful' : True,
+           'top_p'  : 1
+       }
+     )
+   
+   
+
+    
+
+
 def generate_prompt(serverlang, dbprovider, endpoints):
     return """ Write a program in {} with {} to write API endpoints for the following {}.
 
     """.format(serverlang, dbprovider, endpoints)
+    
